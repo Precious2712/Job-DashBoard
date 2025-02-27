6dfa1575ac0f85aad6a25be3bf0d5495a2a9d341
Precious2712 <oluwanifemimoonlight@gmail.com>
2025-02-27 11:42:51 -0800

    correction

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 
 import Apply from "@/applyForm/Apply";
-import AllHeaderComp from "@/Header";
+import AllHeaderComp from "@/Header/AllHeaderComp";
 import { createBrowserRouter } from "react-router-dom"
 
 export const route = createBrowserRouter([
